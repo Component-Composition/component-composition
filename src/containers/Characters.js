8d3dcebb2b396cd652045860261efd5384a26ae2
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-10-31 16:12:30 -0700

    Character prop count per page

--- src/containers/Characters.js
+++ src/containers/Characters.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Cards from '../components/DeckOfCards';
 
 export default class Characters extends Component {
-  state = {
-    data: []
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      data: []
+    };
+  }
+
+  static propTypes = {
+    count: PropTypes.number
+  }
+
+  static defaultProps = {
+    count: 3
   }
 
   componentDidMount() {
-    fetch('https://hey-arnold-api.herokuapp.com/api/v1/characters')
+    const URL = `https://hey-arnold-api.herokuapp.com/api/v1/characters?perPage=${this.props.count}`;
+
+    fetch(URL)
       .then(response => response.json())
       .then(data => this.setState({ data }));
   }
--- src/containers/Gifs.js
+++ src/containers/Gifs.js
@@ -1,23 +1,10 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import Cards from '../components/DeckOfCards';
 
 export default class Gifs extends Component {
-  constructor(props) {
-    super(props);
-  
-    this.state = {
-      data: [],
-      count: props.count
-    };
+  state = {
+    data: []
   }
-
-  static propTypes = {
-    count: PropTypes.number
-  }
-  // state = {
-  //   data: []
-  // }
 
   componentDidMount() {
     fetch('https://hey-arnold-api.herokuapp.com/api/v1/gifs')
